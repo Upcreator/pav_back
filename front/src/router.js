95568d5ec8d7b9e8bba41a9e928572f09184d6c1
EternalDaler <derin.vlad@mail.ru>
2024-05-13 12:10:02 +0300

    Fix

--- front/src/router.js
+++ front/src/router.js
@@ -58,7 +58,7 @@ router.beforeEach((to, from, next) => {
                 next()
             })
             .catch(() => {
-                next({name: 'login'})
+                next({name: 'welcome'})
             })
     }else{
         next();
